963a4743c35743d307b0a78423d3a174d88ab7c7
AnupCh37 <anup37@icloud.com>
2026-01-22 21:43:51 +0545

    some commit

--- Flashconcept/app.py
+++ Flashconcept/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect ,url_for,flash
-
+from forms import RegistrationForm
 app =Flask(__name__)
 app.secret_key="my-secret-key"
 
---
+++ forms/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, redirect ,url_for,flash
+from forms import RegistrationForm
+app =Flask(__name__)
+app.secret_key="my-secret-key"
+
+@app.route("/",methods=["GET","POST"])
+def registe():
+  form=RegistrationForm()
+  if  form.validate_on_submit():
+    name= form.name.data 
+    email = form.email.data
+    flash(f"Welcome,{name}! You Regsitered Sucessfully","success")
+    return redirect(url_for("success"))
+  return render_template("register.html",form=form)
+
+@app.route("/success")
+def success():
+  return render_template("success.html")
